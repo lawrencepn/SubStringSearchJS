f2a774766b3caa48536f14b4af955e6bce9e40b6
lawrencenyakiso <lawrencenyakiso@gmail.com>
2016-04-06 09:40:15 +0200

    Sub String Search JS

--- subStringSearch.js
+++ subStringSearch.js
@@ -1,50 +1,33 @@
 /**
  * Created by lawrencenyakiso on 2016/02/08.
+ * Copyright © 2016 Lawrence Nyakiso
  */
+'use strict'
 exports.substring_search = function (strings, text) {
-
-    var subStringArray = strings.reverse();
-    var subStringArrayLength = subStringArray.length;
-    var cleanString = text.replace(/\s/g, "").trim(); // remove spaces
-    var searchReg = /[^\u0020-\u007F]/g;
-    if (subStringArrayLength < 100) {
-        for (var x = subStringArrayLength - 1; x >= 0; x--) {
-            var match = subStringArray[x].search(searchReg)
-            if (match !== -1 || subStringArray[x].length == 0) {
-                return undefined;
+    var sa = strings.reverse(), sal = sa.length, sr = /[^\u0020-\u007F]/g, res = {}, cs, tl = text.length;
+    if (sal < 100) {
+        for (var x = sal - 1; x >= 0; x--) {
+            cs = sa[x];
+            var ns = text, nsl = tl, csl = sa[x].length, ssr = '(?:\\' + cs + ')', k, m = sa[x].search(sr);
+            if (csl < 100 && m == -1 && csl !== 0) {
+                res[sa[x]] = 0;
+                while (nsl >= csl) {
+                    var is = ns.search(ssr, 'g');
+                    if (is !== -1) {
+                        res[sa[x]]++,k = is + csl, ns = ns.slice(k), nsl = ns.length;
+                    } else {
+                        if (!res[sa[x]]) {
+                            res[sa[x]] = 0;
+                        }
+                        nsl = -1;
+                    }
+                }
+            } else {
+                return undefined
             }
-
         }
-        return doSearch()
+        return res;
     } else {
         return undefined;
     }
-
-    function doSearch() {
-        var res = {};
-        for (var x = subStringArrayLength - 1; x >= 0; x--) {
-            var newString = cleanString;
-            var newStringLength = cleanString.length;
-            var currentSubStringLength = subStringArray[x].length;
-            var occurrence = 0;
-            while (newStringLength >= currentSubStringLength) {
-                var inSubString = newString.search(subStringArray[x])
-                if (inSubString !== -1) {
-                    res[subStringArray[x]] = ++occurrence;
-                    var sliceOff = inSubString + subStringArray[x].length;
-                    newString = newString.slice(sliceOff);
-                    newStringLength = newString.length;
-                } else {
-                    if (!res[subStringArray[x]]) {
-                        res[subStringArray[x]] = 0;
-                    }
-                    newStringLength = -1;
-                }
-            }
-
-        }
-        return res;
-    }
-
-
-}
+};
